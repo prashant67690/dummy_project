fec7556f0afe067e9adc5468106d43c53c67f364
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2021-12-04 11:55:13 +0530

    updated welcome to index

--- index.php
+++ index.php
@@ -4,55 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login page</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="style1.css">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <div id = "frm">
-         <h1>Login</h1>
-         <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">
-             <div class="input-group">
-             <p>
-                 <label> UserName: </label>
-                 <input type = "text" id ="user" name  = "user" />
-             </p>
-             </div>
-               <div class="input-group">
-             <p>
-                 <label> Password: </label>
-                 <input type = "password" id ="pass" name  = "pass" />
-             </p>
-             </div>
-             <p>
-                 <input type =  "submit" id = "btn" value = "Login" />
-             </p>
-         </form>
-     </div>
-
-     <script>
-            function validation()
-            {
-                var id=document.f1.user.value;
-                var ps=document.f1.pass.value;
-                if(id.length=="" && ps.length=="") {
-                    alert("User Name and Password fields are empty");
-                    return false;
-                }
-                else
-                {
-                    if(id.length=="") {
-                        alert("User Name is empty");
-                        return false;
-                    }
-                    if (ps.length=="") {
-                    alert("Password field is empty");
-                    return false;
-                    }
-                }
-            }
-        </script>
+<div class="center">
+  <h1>WELCOME</h1>
+  <?php require 'partials/_nav.php'?>
+</div>
 </body>
 </html>
